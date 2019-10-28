23573edb978068df0ef68d6fdf4fbdfeacd14381
Sam Carey <samcarey99@outlook.com>
2019-10-28 18:05:11 +1100

    First Commit

---
+++ UE4 Rename/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+using System.IO;
+using Path = System.IO.Path;
+
+namespace UE4_Rename
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string OldProjectPath = "";
+        private string OldProjectName = "";
+
+        private string NewProjectName = "TestProject";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Title = "Select .uproject File";
+            openFileDialog.Filter = ".uproject|*.uproject";
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                ProjectPathText.Content = openFileDialog.FileName;
+
+                OldProjectPath = Path.GetDirectoryName(openFileDialog.FileName);
+                OldProjectName = Path.GetFileName(openFileDialog.FileName);
+            }
+        }
+
+        private void UpdateName (string Path)
+        {
+            System.IO.File.Move(OldProjectPath + OldProjectName, OldProjectPath + NewProjectName + ".uproject");    //Rename .uproject File
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
